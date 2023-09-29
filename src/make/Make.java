35dc33a58ff5f3750dbbd6f1b0d98b92d6d5af46
mgmg-zbtn <takeshita6623@gmail.com>
2023-09-29 22:20:11 +0900

    revision up

--- src/make/Make.java
+++ src/make/Make.java
@@ -61,7 +61,7 @@ public class Make {
 		File mainFile = new File(dir.getParent() + "\\index.php");
 		mainFile.createNewFile();
 		
-		Path basePath = Paths.get("src\\PHPTemplate\\index.php");
+		Path basePath = Paths.get("src\\Template\\index.php");
 		String temp = Files.readString(basePath);
 		
 		// replace [Template]
