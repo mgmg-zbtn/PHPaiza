1aa958570ad400085c030b10b3189158814c9382
mgmg-zbtn <takeshita6623@gmail.com>
2023-10-04 00:51:06 +0900

    debug.bat修正

--- debug.bat
+++ debug.bat
@@ -3,20 +3,21 @@ rem 2023/09/23
 rem PHP DEBUG FOR PAIZA
 
 rem sample
-rem debug.bat P000 P*** 1
+rem debug.bat Z000 Z*** 1
 
 rem %1 P000
 rem %2 P***
-rem %3 No, of testcase : 1 ~ N
+rem %3 1 ~ N (testcase No.)
 
 rem 指定した番号のテストケース実行
 php ./src/%1/%2/index.php < ./src/%1/%2/testcase/case%3.txt
 @echo on
 
-rem 実行結果をログ出力
 @echo off
+rem 実行結果をログファイルへ出力
 php ./src/%1/%2/index.php < ./src/%1/%2/testcase/case%3.txt > ./src/%1/%2/testcase/log%3.txt
 
+echo;
 rem 出力結果と正答の検査
-java -jar PaizaDebug.jar ./src/%1/%2/testcase/log%3.txt ./src/%1/%2/estcase/ans%3.txt
+java -jar PaizaCompare.jar ./src/%1/%2/testcase/log%3.txt ./src/%1/%2/testcase/ans%3.txt
 @echo on
